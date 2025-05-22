1addde14c36bdf975776ac5658e46667a7455ef7
Francisco Giovanelli <franciscogiovanelli1891@gmail.com>
2025-05-22 17:44:39 -0300

    feat: add vulnerable SQL query

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ db.serialize(() => {
   db.run("INSERT INTO users VALUES (1, 'alice'), (2, 'bob')");
 });
 
-
+app.get('/users', (req, res) => {
+  const username = req.query.username;
+  const query = `SELECT * FROM users WHERE username = '${username}'`;
+  
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json({ users: rows });
+  });
+});
 
 app.listen(3000, () => console.log('App running on http://localhost:3000'));
