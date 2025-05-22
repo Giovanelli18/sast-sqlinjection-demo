6acdcdfe633e14f9e6a9d1004893329771959197
Francisco Giovanelli <franciscogiovanelli1891@gmail.com>
2025-05-22 00:24:39 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+const db = new sqlite3.Database(':memory:');
+
+db.serialize(() => {
+  db.run("CREATE TABLE users (id INT, username TEXT)");
+  db.run("INSERT INTO users VALUES (1, 'alice'), (2, 'bob')");
+});
+
+app.get('/users', (req, res) => {
+  const username = req.query.username;
+  // ⚠️ Vulnerabilidad: SQL Injection
+  const query = `SELECT * FROM users WHERE username = '${username}'`;
+  
+  db.all(query, [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json({ users: rows });
+  });
+});
+
+app.listen(3000, () => console.log('App running on http://localhost:3000'));
