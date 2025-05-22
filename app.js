4683974a9c10d9f660a8d182977ffc8df47bfefc
Francisco Giovanelli <franciscogiovanelli1891@gmail.com>
2025-05-22 16:03:01 -0300

    preparacion

--- app.js
+++ app.js
@@ -8,17 +8,6 @@ db.serialize(() => {
   db.run("INSERT INTO users VALUES (1, 'alice'), (2, 'bob')");
 });
 
-app.get('/users', (req, res) => {
-  const username = req.query.username;
-  // ⚠️ Vulnerabilidad: SQL Injection
-  const query = `SELECT * FROM users WHERE username = '${username}'`;
-  
-  db.all(query, [], (err, rows) => {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-    res.json({ users: rows });
-  });
-});
+
 
 app.listen(3000, () => console.log('App running on http://localhost:3000'));
